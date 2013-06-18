633274661f7cbc6f531752510b89778e8101066c
Tim Anema <tim@zenapsis.com>
2013-06-18 16:16:01 -0400

    fixed the png mime serving

--- src/timanema.com/app/mimes/png.go
+++ src/timanema.com/app/mimes/png.go
@@ -3,11 +3,18 @@ package mimes
 import (
   "github.com/robfig/revel"
   "net/http"
+  "io"
+  "encoding/base64"
+  "strings"
 )
 
 type Png string
 
 func (r Png) Apply(req *revel.Request, resp *revel.Response) {
   resp.WriteHeader(http.StatusOK, "image/png")
-  resp.Out.Write([]byte(r))
+  data := strings.Split(string(r), ",")
+  if len(data) > 1 {
+    png := base64.NewDecoder(base64.StdEncoding, strings.NewReader(data[1]))
+    io.Copy(resp.Out, png)
+  }
 }
