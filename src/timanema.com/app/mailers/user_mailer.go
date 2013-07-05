af9c30f2b251e9d8475aba2d3d4c9b187c27106c
Tim Anema <tim@zenapsis.com>
2013-07-05 14:13:34 -0400

    using the revel_mailer library now, really simple mailers now

--- src/timanema.com/app/mailers/user_mailer.go
+++ src/timanema.com/app/mailers/user_mailer.go
@@ -1,11 +1,15 @@
 package mailers
 
+import (
+  "github.com/tanema/revel_mailer"
+)
+
 type UserMailer struct {
-  Mailer
+  revel_mailer.Mailer
 }
 
 func (u UserMailer) SendReport(reported_id string) error {
-  return u.Send(H{
+  return u.Send(revel_mailer.H{
             "subject": "a signature has been reported",
             "to": []string{"timanema@gmail.com"},
             "reported_id": reported_id,
